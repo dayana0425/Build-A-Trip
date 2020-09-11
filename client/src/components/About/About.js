0cd8c0ffe15cc0431fdf7ce9baf67351048832c8
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-09-10 21:28:31 -0600

    change the position of close button close#123

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -13,10 +13,23 @@ export default class About extends Component {
         <Container id="about">
           <Row>
             <Col>
+            <Row>
+              <Col columnWidth = '2'>
               <h1> About: {CLIENT_TEAM_NAME} </h1>
+              </Col>
+              <Col id="closeAbout" xs='auto' >
+                <Button color="primary" onClick={this.props.closePage} xs={1}>
+                    Close
+                </Button>
+              </Col>
+            </Row>
                 <h2> Mission Statement </h2>
 
-                <p> Team Hexadecimal is comprised of aspiring Computer Scientists who are dedicated to building not only amazing software projects but building amazing software projects the right way by applying Clean Code principles to the code we write and utilizing scrum development as our process. </p>
+                <p> Team Hexadecimal is comprised of aspiring Computer Scientists
+                who are dedicated to building not only amazing software projects
+                but building amazing software projects the right way by applying
+                Clean Code principles to the code we write and utilizing scrum
+                development as our process. </p>
 
                 <h2> Team Members </h2>
 
@@ -27,11 +40,9 @@ export default class About extends Component {
                 <ChenAbout name = "Chen Wang" />
 
                 <MattAbout name = "Matt Vildibill" />
-            </Col>
-            <Col id="closeAbout" xs='auto' >
-              <Button color="primary" onClick={this.props.closePage} xs={1}>
-                Close
-              </Button>
+                <Button color="primary" onClick={this.props.closePage} xs={1}>
+                                Close
+                              </Button>
             </Col>
           </Row>
         </Container>
