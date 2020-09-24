1ee5464e0cb09e60d8416981dd913023db2e605a
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-09-24 12:34:45 -0600

    add database and testfile for findrequest

--- server/src/main/java/com/tco/requests/RequestFind.java
+++ server/src/main/java/com/tco/requests/RequestFind.java
@@ -10,11 +10,12 @@ import org.slf4j.LoggerFactory;
 public class RequestFind extends RequestHeader{
 
     private String match;            //an alphanumeric pattern used to find geographic locations
-    private Integer limit;          //the maximun number of matching places that may be return
-    private Integer found;          //the total number of matching places in the data sources
+    private int limit;          //the maximun number of matching places that may be return
+    private int found;          //the total number of matching places in the data sources
     private ArrayList places;       //list of places found
     private final transient Logger log = LoggerFactory.getLogger(RequestConfig.class);
-    private FindDatabase fdb;
+
+    private String tunnel;
 
 
     public RequestFind(){       //default constructor
@@ -34,16 +35,22 @@ public class RequestFind extends RequestHeader{
     }
 
     @Override
-    public void buildResponse() throws BadRequestException {
-      this.found = fdb.getCount();
-      this.places = fdb.getPlaces();
+    //public void buildResponse() throws BadRequestException {
+    public void buildResponse(){
+        FindDatabase fdb = new FindDatabase(match,limit);
+        fdb.connect2DB();
+        tunnel = fdb.getUseDatabaseTunnel();
+        this.found = fdb.getCount();
+        this.places = fdb.getPlaces();
     }
 
-    public Integer getLimit() {return limit;}
+    public int getLimit() {return limit;}
 
     public String getMatch() {return match;}
 
-    public Integer getFound() {return found;}
+    public int getFound() {return found;}
 
     public ArrayList getPlaces() {return places;}
+
+    public String getTunnel(){return tunnel;}
 }
