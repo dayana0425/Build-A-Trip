5ec5eaa47802b0b7ebf80601098137b645f5e9a4
Daiana Bilbao <dbilbao@cs.colostate.edu>
2020-09-29 20:30:24 -0600

    Fixed Database Query and limit/found and also the constructors so that limit and match are optional fields

--- server/src/main/java/com/tco/requests/FindDatabase.java
+++ server/src/main/java/com/tco/requests/FindDatabase.java
@@ -4,27 +4,21 @@ import org.eclipse.jetty.client.util.StringContentProvider;
 
 import java.lang.Integer;
 import java.lang.String;
+import java.sql.*;
 import java.util.ArrayList;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import  java.sql.ResultSet;
 import java.util.Queue;
 
 
 public class FindDatabase {
-
     private String match;
     private int limit;
     private int count;
-    private ArrayList<Place> places;
+    private ArrayList<Place> places = new ArrayList<Place>();;
     private String useTunnel;
     public FindDatabase(String match, int limit){
         this.match = match;
         this.limit = limit;
     }
-
 
     public void connect2DB() {
         String isTravis = System.getenv("TRAVIS");
@@ -46,25 +40,22 @@ public class FindDatabase {
             DB_USER = "cs314-db";
             DB_PASSWORD = "eiK5liet1uej";
         }
-        String limitation = Integer.toString(limit);
+
         String QUERY =
                 "SELECT world.name,world.id,world.type,world.latitude,world.longitude,world.municipality,world.altitude " +
                 "FROM world INNER JOIN continent INNER JOIN region INNER JOIN country " +
                 "WHERE world.continent = continent.id AND world.iso_region = region.id AND world.iso_country = country.id AND " +
                 "(world.name LIKE '%" + match + "%' OR world.municipality LIKE '%" + match + "%' OR continent.name LIKE '%" + match + "%' OR region.name LIKE '%" + match + "%') " +
-                "ORDER BY world.name desc LIMIT " + limit + ";";
+                "ORDER BY world.name asc;";
             try ( // connect to the database and query
               Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
               Statement query = conn.createStatement();
               ResultSet results = query.executeQuery(QUERY);
+
             )
             {
-                System.out.println(QUERY);
-                count = 0;
-                places = new ArrayList<Place>();
                 while (results.next()) {
                   Place p = new Place(
-
                           results.getString("name"),
                           results.getString("latitude"),
                           results.getString("longitude"),
@@ -73,8 +64,13 @@ public class FindDatabase {
                           results.getString("municipality"),
                           results.getString("type")
                   );
-                  count++;
                   places.add(p);
+                  count++;
+                }
+
+                if(limit > 0){
+                    ArrayList<Place> newPlaces = new ArrayList<Place>(places.subList(0,limit));
+                    places = newPlaces;
                 }
             }
             catch(Exception e){
--- server/src/main/java/com/tco/requests/RequestFind.java
+++ server/src/main/java/com/tco/requests/RequestFind.java
@@ -12,34 +12,37 @@ public class RequestFind extends RequestHeader{
     private String match; //an alphanumeric pattern used to find geographic locations
     private int limit; //the maximun number of matching places that may be return
     private int found; //the total number of matching places in the data sources
-    private ArrayList places; //list of places found
+    private ArrayList places = new ArrayList<Place>(); //list of places found
     private final transient Logger log = LoggerFactory.getLogger(RequestFind.class);
 
     private String tunnel;
 
 
-    public RequestFind(){       //default constructor
+    public RequestFind(){ //default constructor
         this.requestVersion = RequestHeader.CURRENT_SUPPORTED_VERSION;
         this.requestType = "find";
+        this.match = "Dave";
     }
 
+    public RequestFind(String match) {
+        this();
+        this.match = match;
+        this.limit = 0;
+    }
 
     public RequestFind(String match, int limit){
         this();
         this.match = match;
-        this.limit = limit;     // this means the client doesn't give a limitation for how many do they want
-        if(this.limit == 0) {
-            this.limit = 10;
-        }
-
+        this.limit = limit; // this means the client doesn't give a limitation for how many do they want
     }
+
 
     @Override
     //public void buildResponse() throws BadRequestException {
     public void buildResponse(){
         FindDatabase fdb = new FindDatabase(match,limit);
         fdb.connect2DB();
-        tunnel = fdb.getUseDatabaseTunnel();
+        //tunnel = fdb.getUseDatabaseTunnel();
         this.found = fdb.getCount();
         this.places = fdb.getPlaces();
     }
@@ -52,5 +55,5 @@ public class RequestFind extends RequestHeader{
 
     public ArrayList getPlaces() {return places;}
 
-    public String getTunnel(){return tunnel;}
+    //public String getTunnel(){return tunnel;}
 }
