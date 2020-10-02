78a55501872929a7c8d9faadfde6c50a2df4c998
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-10-01 19:54:10 -0600

    Change the Label for FindDistance.js

--- client/src/components/Atlas/FindDistance.js
+++ client/src/components/Atlas/FindDistance.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Col, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import MyLabel from './FindLabel.js';
 const findDistanceButtonStyle = {
     marginBottom: 10
 }
@@ -9,14 +9,8 @@ export default () => {
     return(
     <Col sm="30">
         <Form>
-            <FormGroup>
-                <Label for="Location_1">Location 1</Label>
-                <Input type="Location_1" name="Location_1" id="ExampleLocation_1" placeholder="Enter Longitude, Latitude" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="Location_2">Location 2</Label>
-                <Input type="Location_2" name="Location_2" id="ExampleLocation_2" placeholder="Enter Longitude, Latitude" />
-            </FormGroup>
+            <MyLabel labelName = "Location 1" inputName = "Location_1"/>
+            <MyLabel labelName = "Location 2" inputName = "Location_2"/>
         </Form>
         <Button color="primary" style = {findDistanceButtonStyle}>Find Distance</Button>{' '}
     </Col>
