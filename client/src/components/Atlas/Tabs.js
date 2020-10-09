faee0e36e02e114af0c89ffd471065a1f48b4885
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-10-08 20:00:09 -0600

    clean code for Tabs.js

--- client/src/components/Atlas/Tabs.js
+++ client/src/components/Atlas/Tabs.js
@@ -1,32 +1,65 @@
-import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
-import classnames from 'classnames';
+import React from 'react';
+import { TabContent, TabPane, } from 'reactstrap';
+import {Tabs, Tab, AppBar} from "@material-ui/core";
 import FindDistance from "./FindDistance.js";
 import WhereIs from "./WhereIs"
 import FindPlaces from "./FindPlaces"
 
-const Example = () => {
-    const [activeTab, setActiveTab] = useState('1');
 
-    const toggle = tab => {
-        if(activeTab !== tab) setActiveTab(tab);
-    }
+const NavTabs = () =>  {
+   const [selectedTab, setSelectedTab] = React.useState(0);
+
+    const handleChange = (event,newTabValue) => {
+        setSelectedTab(newTabValue);
+    };
 
     return (
-        <div>
+       <>
+        < Tabs value = {selectedTab} onChange = {handleChange}>
+            <Tab label = "Find Distance"/>
+            <Tab label = "Where Is?"/>
+            <Tab label = "Find Places"/>
+        </Tabs>
+        {selectedTab === 0 && <FindDistance/>}
+        {selectedTab === 1 && <WhereIs/>}
+        {selectedTab === 2 && <FindPlaces/>}
+        </>
+        );
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/*<div>
             <Nav tabs>
                 <NavItem>
                     <NavLink
                         className={classnames({ active: activeTab === '1' })}
-                        onClick={() => { toggle('1'); }}
+                        onClick={() => { toggleTab('1'); }}
                     >
                         Find Distance
                     </NavLink>
                 </NavItem>
+
                 <NavItem>
                     <NavLink
                         className={classnames({ active: activeTab === '2' })}
-                        onClick={() => { toggle('2'); }}
+                        onClick={() => { toggleTab('2'); }}
                     >
                         Where Is?
                     </NavLink>
@@ -34,29 +67,40 @@ const Example = () => {
                 <NavItem>
                     <NavLink
                         className={classnames({ active: activeTab === '3' })}
-                        onClick={() => { toggle('3'); }}
+                        onClick={() => { toggleTab('3'); }}
                     >
                         Find Places
                     </NavLink>
                 </NavItem>
+
             </Nav>
 
             <TabContent activeTab={activeTab}>
-                <TabPane tabId="1">
+                <TabPane tabId='1'>
                         <h5> Find Distance </h5>
-                        <FindDistance> </FindDistance>
+                        <FindDistance/>
                 </TabPane>
+             </TabContent>
+             <TabContent activeTab={activeTab}>
                 <TabPane tabId="2">
                         <h5> Where is? </h5>
-                        <WhereIs> </WhereIs>
+                        <WhereIs/>
                 </TabPane>
+                 </TabContent>
+                 <TabContent activeTab={activeTab}>
                 <TabPane tabId="3">
                         <h5> Find Places </h5>
-                        <FindPlaces> </FindPlaces>
+                        <FindPlaces/>
+                </TabPane>
+                <TabPane tabId="4">
+                       <h5> test </h5>
+                        <FindDistance/>
                 </TabPane>
             </TabContent>
         </div>
     );
-}
 
-export default Example;
+};*/}
+
+export default NavTabs;
+
