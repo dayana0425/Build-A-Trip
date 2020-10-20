15eb3581717cf93c8499ec19b8537620a0ea18dd
Daiana Bilbao <dbilbao@cs.colostate.edu>
2020-10-20 16:40:40 -0600

    fixed code climate issue

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -78,49 +78,36 @@ export default class Atlas extends Component {
                 <Container>
                     <Row>
                         <Col sm={12} md={{size: 10, offset: 1}}>
-                            <h1> Build A Trip: {this.state.tripName} </h1>
+                            <h1> Build A Trip: {this.state.tripName} </h1> {/* Might move this somewhere else later*/}
                             {this.renderTripName()}
-                            <Nav tabs>
-                                <NavItem>
-                                    <NavLink
-                                        className={classnames({ active: this.state.activeTab === '1' })}
-                                        onClick={() => { this.toggle('1'); }}>
-                                        Add Locations
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink
-                                        className={classnames({ active: this.state.activeTab=== '2' })}
-                                        onClick={() => { this.toggle('2'); }}>
-                                        Search Places
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                            <TabContent activeTab={this.state.activeTab}>
-                                <TabPane tabId="1">
-                                    { this.state.activeTab == 1 }
-                                    <FindDistance/>
-                                </TabPane>
-                                <TabPane tabId="2">
-                                    { this.state.activeTab == 2 }
-                                    { this.renderFindPlaces() }
-                                </TabPane>
-                            </TabContent>
+                            {this.renderTabs()}
                             {this.renderLeafletMap()}
-                            <Button color="primary" style={this.buttonStyleCurrLocation}
-                                    onClick={() => this.requestCurrentLocation()}>
-                                    Add Current Location
-                            </Button>
-                            <Button color="primary" style={this.buttonStyleClear}
-                                    onClick={() => this.clearAllMarkers()}>
-                                    Clear
-                            </Button>
+                            {this.renderMapButtons()}
                         </Col>
                     </Row>
                 </Container>
             </div>
         );
     }
+
+    /* MAP BUTTONS */
+
+    renderMapButtons(){
+        return(
+            <div>
+            <Button color="primary" style={this.buttonStyleCurrLocation}
+                    onClick={() => this.requestCurrentLocation()}>
+                Add Current Location
+            </Button>
+            <Button color="primary" style={this.buttonStyleClear}
+                    onClick={() => this.clearAllMarkers()}>
+                Clear
+            </Button>
+            </div>
+        );
+    }
+
+    /* END OF MAP BUTTONS */
 
     /* START OF TRIP COMPONENT */
 
@@ -145,7 +132,6 @@ export default class Atlas extends Component {
         });
     }
 
-
     /* END OF TRIP COMPONENT */
 
     /* START OF TAB COMPONENT */
@@ -154,6 +140,39 @@ export default class Atlas extends Component {
         if (this.state.activeTab !== tab) {
             this.setState({activeTab: tab});
         }
+    }
+
+    renderTabs(){
+        return(
+            <div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '1' })}
+                            onClick={() => { this.toggle('1'); }}>
+                            Add Locations
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab=== '2' })}
+                            onClick={() => { this.toggle('2'); }}>
+                            Search Places
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        { this.state.activeTab == 1 }
+                        <FindDistance/>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        { this.state.activeTab == 2 }
+                        { this.renderFindPlaces() }
+                    </TabPane>
+                </TabContent>
+            </div>
+        )
     }
 
     /* END OF TAB COMPONENT */
