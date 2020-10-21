49dcffdab769ebdf23f37c6d06ed84ae7efce08c
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-10-21 00:22:00 -0600

    the distance server doesn't have too many problems. the only thing is that it copied wrong schema for request, I have changed it now. close #370

--- server/src/main/java/com/tco/requests/RequestDistance.java
+++ server/src/main/java/com/tco/requests/RequestDistance.java
@@ -22,20 +22,20 @@ public class RequestDistance extends RequestHeader {
 
     public RequestDistance(Double radius, String lat1, String lon1, String lat2, String lon2) {
         this();
-        this.distance = null;
-        this.earthRadius = radius;
         this.place1 = new HashMap();
         this.place1.put("latitude", lat1);
         this.place1.put("longitude", lon1);
         this.place2 = new HashMap();
         this.place2.put("latitude", lat2);
         this.place2.put("longitude", lon2);
+        this.distance = null;
+        this.earthRadius = radius;
     }
 
     @Override
     public void buildResponse() {
-        this.distance = calculate(this.place1, this.place2, this.earthRadius);
         log.trace("buildResponse -> {}", this);
+        this.distance = calculate(this.place1, this.place2, this.earthRadius);
     }
 
     static boolean validateCoords(double lat, double lon){
