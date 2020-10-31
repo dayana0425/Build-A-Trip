95dd9ea3e96849f5b81ffd4a27a70065d1800c33
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-10-31 12:18:47 -0600

    fix the boundary

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -130,19 +130,16 @@ export default class Atlas extends Component {
     }
 
     getBounds(coords){
-        console.log(coords)
         let sortedMarkerPositions = [coords]
         if (this.state.markerPositions.length != 0) {
-            sortedMarkerPositions = [...this.state.markerPositions].sort((a, b) => (a.lng > b.lng) ? 1 : -1);
-            console.log(this.state.markerPositions)
-                if (sortedMarkerPositions.length == 1) {
-                     this.setState({map_center:[sortedMarkerPositions[0].lat, sortedMarkerPositions[0].lng]});
-                     this.setState({zoom:15});
-                }
-                else {
-                     console.log(sortedMarkerPositions)
-                     this.setState({fit_bounds:L.latLngBounds(sortedMarkerPositions[0], sortedMarkerPositions[sortedMarkerPositions.length - 1])});
-                }
+            sortedMarkerPositions = [...sortedMarkerPositions,...this.state.markerPositions].sort((a, b) => (a.lng > b.lng) ? 1 : -1);
+//                if (sortedMarkerPositions.length == 1) {
+//                     this.setState({map_center:[sortedMarkerPositions[0].lat, sortedMarkerPositions[0].lng]});
+//                     this.setState({zoom:15});
+//                     console.log('gere')
+//                }
+//                else
+                  this.setState({fit_bounds:L.latLngBounds(sortedMarkerPositions[0], sortedMarkerPositions[sortedMarkerPositions.length - 1])});
              }
             else {
                this.setState({map_center: MAP_CENTER_DEFAULT});
