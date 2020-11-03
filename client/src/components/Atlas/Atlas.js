93b91b3e97e6150fb892547015e4b27070fb15b2
Daiana Bilbao <dbilbao@cs.colostate.edu>
2020-11-03 09:33:20 -0700

    added empty line

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -129,6 +129,7 @@ export default class Atlas extends Component {
         this.setState({ markerPositions: [...this.state.markerPositions, coords]});
         this.setState({ placesForItinerary: [...this.state.placesForItinerary, {name: placeName, latitude: coords.lat + '', longitude: coords.lng + ''}]});
         //this.getBounds(coords);
+
     }
 
 
