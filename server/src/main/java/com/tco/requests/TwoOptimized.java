62d6caf629939fbda9adee0d1b5b5a0a420b4a55
Xinyi-Wang99 <Xinyi.Wang@colostate.edu>
2020-11-11 23:26:55 -0700

    finish NearestNeighbor.java

--- server/src/main/java/com/tco/requests/TwoOptimized.java
+++ server/src/main/java/com/tco/requests/TwoOptimized.java
@@ -10,24 +10,30 @@ public class TwoOptimized {
     private long [][] distances; // the distance between cities
     private long roundTripDistance; // will hold the resulting optimized round trip distance
     private int size; // saves the size of the matrix
-    private boolean improvement = false;
 
 
-    TwoOptimized(int size, long[][] distances) throws BadRequestException {
+    TwoOptimized(int size, long[][] distances, int[] tour) throws BadRequestException {
         /* INITIALIZING */
         this.size = size;
         this.distances = distances;
-        this.tour = new int[size];
-        this.populateTour();
+        this.tour = tour;
     }
 
-    public void populateTour(){
-        for(int i=0; i<size; i++){
-            tour[i] = i;
+    public void optimization(){
+        boolean improvement = true;
+        while(improvement)
+        improvement = false;
+        for(int i =0; i< size-2; i++){
+            for(int k = i+2; k<size; k++){
+                long delta = distances[tour[i]][tour[i+1]] + distances[tour[k]][tour[(k+1)%size]] - distances[tour[i]][tour[k]]- distances[tour[i+1]][tour[(k+1)%size]];
+                if(delta > 0){
+                    //reverse(i,k);
+
+                }
+            }
         }
+
     }
-
-
 
 
     //Accessor Method: returns the optimizedTour result
